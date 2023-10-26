249134056cc44973e8252e83a5dc9a92a2b16705
JojoDuke <jojoamankwa@gmail.com>
2023-10-26 16:57:49 +0000

    navbar done

--- FiftyFiftyFunding/fiftyfiftyfunding/components/Navbar.js
+++ FiftyFiftyFunding/fiftyfiftyfunding/components/Navbar.js
@@ -3,10 +3,10 @@ import Image from 'next/image';
 
 function Navbar() {
 
-    const navItems = ['OUR GOAL', 'HOW IT WORKS', 'MEET THE TEAM', 'STRUCTURE', 'APPLY NOW'];
+    const navItems = ['OUR GOAL', 'HOW IT WORKS', 'MEET THE TEAM', 'STRUCTURE'];
 
   return (
-    <nav className="py-4 px-6 transparent">
+    <nav className="py-4 px-6 transparent z-10">
         <div className='container mx-auto flex items-center justify-between'>
             {/* Logo */}
             <div className="font-bold text-xl">
@@ -16,12 +16,19 @@ function Navbar() {
             </div>
 
             {/* Nav Items */}
-            <div className="ml-auto space-x-4 flex flex-row">
+            <div className="ml-auto space-x-4 flex flex-row justify-center items-center">
                 {navItems.map((item, index) => (
                     <Link key={index} href={`/${item.replace(/\s+/g, '-').toLowerCase()}`}>
-                        <p className="text-gray-700 hover:text-black transition">{item}</p>
+                        <p className="text-white hover:text-black transition">{item}</p>
                     </Link>
                 ))}
+                <div className="border border-white text-white font-bold py-2 px-4 rounded inline-flex items-center hover:bg-white hover:text-black cursor-pointer transition">
+                    <span className="mr-2">APPLY NOW</span>
+                    <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" d="M6.293 9.293a1 1 0 011.414 0L12 13.586V6a1 1 0 112 0v7.586l4.293-4.293a1 1 0 111.414 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                    </svg>
+                </div>
+
             </div>
         </div>
     </nav>
