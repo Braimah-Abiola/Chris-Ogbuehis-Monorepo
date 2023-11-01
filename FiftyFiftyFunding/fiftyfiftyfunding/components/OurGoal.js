82a717214fe423e1c24b7386b209ca858e3a9970
JojoDuke <jojoamankwa@gmail.com>
2023-11-01 00:24:22 +0000

    margin right

--- FiftyFiftyFunding/fiftyfiftyfunding/components/OurGoal.js
+++ FiftyFiftyFunding/fiftyfiftyfunding/components/OurGoal.js
@@ -30,7 +30,7 @@ function OurGoal() {
             </p>
             <PopupButton id="netPyDqo">
               <div className="border border-white text-white font-bold py-2 px-4 rounded inline-flex items-center hover:bg-white hover:text-black cursor-pointer transition">
-                  <span className="mr-2">APPLY NOW →</span>
+                  <span className="mr-3">APPLY NOW →</span>
               </div>
             </PopupButton>
         </div>
