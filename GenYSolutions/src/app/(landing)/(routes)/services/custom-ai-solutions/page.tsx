b8ee0158a719c045c58419c5602f87f51f68c169
Braimah-Abiola <braimahabiola.official@gmail.com>
2023-11-05 23:15:23 +0000

    Fixing build error

--- GenYSolutions/src/app/(landing)/(routes)/services/custom-ai-solutions/page.tsx
+++ GenYSolutions/src/app/(landing)/(routes)/services/custom-ai-solutions/page.tsx
@@ -29,6 +29,23 @@ const CustomAISolutions = () => {
     }, 2000);
   }, []);
 
+  const body = useRef(null);
+
+  const isInView = useInView(body, { once: true, margin: "75%" });
+
+  const animation = {
+    initial: { y: "100%" },
+
+    enter: (i: number) => ({
+      y: "0",
+      transition: {
+        duration: 0.75,
+        ease: [0.33, 1, 0.68, 1],
+        delay: 2.5,
+      },
+    }),
+  };
+
   const [openQuestion, setOpenQuestion] = useState<number | null>(null);
 
   const handleQuestionClick = (index: number) => {
@@ -52,7 +69,18 @@ const CustomAISolutions = () => {
       <LandingNav />
       <MaxWidthWrapper className="flex flex-col items-center justify-center text-center h-screen bg-black">
         <div className="z-10 items-center justify-center flex flex-col -mt-10 md:-mt-0">
-          <MaskText />
+          <div ref={body}>
+            <div className="lineMask z-20">
+              <motion.h1
+                className="max-w-5xl text-5xl font-semibold md:text-6xl lg:text-[72px] lg:leading-[1.2] text-white md:-mt-0"
+                variants={animation}
+                initial="initial"
+                animate={isInView ? "enter" : ""}
+              >
+                Custom AI Solutions
+              </motion.h1>
+            </div>
+          </div>
           <p className=" mt-2 md:mt-5 max-w-prose text-lg text-zinc-200 text-center">
             We aim for ingenuity that leads to emotional fulfillment in every
             project.
@@ -343,37 +371,3 @@ const CustomAISolutions = () => {
 };
 
 export default CustomAISolutions;
-
-export function MaskText() {
-  const body = useRef(null);
-
-  const isInView = useInView(body, { once: true, margin: "75%" });
-
-  const animation = {
-    initial: { y: "100%" },
-
-    enter: (i: number) => ({
-      y: "0",
-      transition: {
-        duration: 0.75,
-        ease: [0.33, 1, 0.68, 1],
-        delay: 2.5,
-      },
-    }),
-  };
-
-  return (
-    <div ref={body}>
-      <div className="lineMask z-20">
-        <motion.h1
-          className="max-w-5xl text-5xl font-semibold md:text-6xl lg:text-[72px] lg:leading-[1.2] text-white md:-mt-0"
-          variants={animation}
-          initial="initial"
-          animate={isInView ? "enter" : ""}
-        >
-          Custom AI Solutions
-        </motion.h1>
-      </div>
-    </div>
-  );
-}
